728ba7e554aec7374d72fc81d1b32474a76b6674
ChrisACorrea <christopher.a.c@outlook.com>
2020-01-30 23:56:37 -0300

    Criado funcionalidade de calculo do INSS

---
+++ calculoIRPF/src/main/java/br/com/techsoft/calculoirpf/Inss.java
@@ -0,0 +1,43 @@
+package br.com.techsoft.calculoirpf;
+
+public class Inss {
+    private final double SALARIO_FAIXA_1 = 1751.81;
+    private final double SALARIO_FAIXA_2 = 2919.72;
+    private final double SALARIO_FAIXA_3 = 5839.45;
+    private final double DESCONTO_MAXIMO = this.SALARIO_FAIXA_3 * this.aliquota;
+    private final double salario;
+    private double aliquota;
+    private double impostoINSS;
+
+    public Inss(double salario) {
+        this.salario = salario;
+        setAliquota();
+        calculaImposto();
+    }
+
+    private void setAliquota() {
+        if (this.salario <= this.SALARIO_FAIXA_1) {
+            this.aliquota = 0.08;
+        } else if (this.salario <= this.SALARIO_FAIXA_2) {
+            this.aliquota = 0.09;
+        } else {
+            this.aliquota = 0.11;
+        }
+    }
+
+    private void calculaImposto() {
+        if (this.salario <= this.SALARIO_FAIXA_3) {
+            this.impostoINSS = this.salario * this.aliquota;
+        } else {
+            this.impostoINSS = this.DESCONTO_MAXIMO;
+        }
+    }
+
+    public double calculaSalario(){
+        return this.salario - this.impostoINSS;
+    }
+
+    public double getImpostoINSS(){
+        return this.impostoINSS;
+    }
+}
