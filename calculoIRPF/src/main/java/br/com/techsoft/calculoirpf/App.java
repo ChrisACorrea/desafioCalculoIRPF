21df0051025ba825e05dc6f6b6b13d5db2d92e6e
ChrisACorrea <christopher.a.c@outlook.com>
2020-02-03 18:04:06 -0300

    Versão para envio

--- calculoIRPF/src/main/java/br/com/techsoft/calculoirpf/App.java
+++ calculoIRPF/src/main/java/br/com/techsoft/calculoirpf/App.java
@@ -1,14 +1,22 @@
 package br.com.techsoft.calculoirpf;
 
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        double salario = 3000;
-        int quantidadeDependentes = 2;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Por favor digite seu salário: ");
+        double salario = teclado.nextDouble();
+        System.out.print("Agora digite quantos dependentes você possui: ");
+        int quantidadeDependentes = teclado.nextInt();
         Imposto imposto = new Imposto(salario, quantidadeDependentes);
+        teclado.close();
 
+        System.out.println();
+        System.out.println("Aqui está o calculo do seu imposto.");
+        System.out.println();
         System.out.println("INSS: " + imposto.getImpostoINSS());
         System.out.println("IRPF: " + imposto.getImpostoIRPF());
         System.out.println("Desconto total: " + imposto.getDescontoTotal());
